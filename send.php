6aee922c4e3ab6f9614a05ee8249dbcd433e3b54
xuan1989 <xuan1989@users.noreply.github.com>
2019-12-19 17:55:02 +0800

    Update send.php

--- send.php
+++ send.php
@@ -1,10 +1,8 @@
 <?php
-use google\appengine\api\mail\Message;
-
+include_once('mail/class.phpmailer.php');
 class Send {
 	private $token = '57744c1def01da4f91cb3153927c6234';
 	private $error;
-
 	private $config = [
 		// 'mail_type'         => 'SMTP',
 		// 'mail_host'         => '',
@@ -12,71 +10,72 @@ class Send {
 		// 'mail_user'         => '',
 		// 'mail_pass'         => '',
 		// 'mail_timeout'      => 10,
-
 		'sender_to_mail'    => '',
 		'sender_from_mail'  => '',
 		'sender_author'     => '',
 		'sender_subject'    => '',
 		'sender_content'    => ''
 	];
-
 	private $not_required = [
 		// 'mail_type',
 		// 'mail_port',
 		// 'mail_timeout'
 	];
-
 	public function index() {
 		if (!$this->validate()) {
 			echo $this->error;
 			exit;
 		}
-
 		print_r($this->config);
-
 		$this->sendOut();
 	}
-
 	private function validate() {
-		if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-			$this->error = 'hello world!';
-			return false;
+		if (empty($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] != 'POST') {
+			return $this->error('hello world!');
 		}
-
 		if (empty($_POST['token']) || $_POST['token'] != $this->token) {
-			$this->error = 'hello world!';
-			return false;
+			return $this->error('hello world!');
 		}
-
 		foreach ($this->config as $key => $value) {
 			if (empty($_POST[$key]) && !in_array($key, $this->not_required)) {
-				$this->error = 'param error: '.$key.' empty';
-				return false;
+				return $this->error('param error: '.$key.' empty');
 			}
-
 			if (!empty($_POST[$key])) {
 				$this->config[$key] = $_POST[$key];
 			}
 		}
-
 		return true;
 	}
-
+	private function error($message) {
+		$this->error = $message;
+		return false;
+	}
 	private function sendOut() {
 		try {
-		    $message = new Message();
-		    $message->setSender(strtolower($this->config['sender_from_mail']));
-		    $message->addTo(strtolower($this->config['sender_to_mail']));
-		    $message->setSubject(html_entity_decode($this->config['sender_subject'], ENT_QUOTES, 'UTF-8'));
-		    $message->setHtmlBody($this->config['sender_content']);
-		    // $message->addAttachment('image.jpg', $image_data, $image_content_id);
-		    $message->send();
-		    echo '[ok]';
-		} catch (InvalidArgumentException $e) {
-		    echo '[error]';
+			$mail = new PHPMailer();
+			$mail->IsSMTP();  //telling the class to use SMTP
+			$mail->isHTML(true);
+			$mail->CharSet      = 'UTF-8'; //设置邮件的字符编码
+			$mail->Host         = $_POST['smtp_host']; //also tried "relay-hosting.secureserver.net"
+			$mail->SMTPAuth     = true;
+			$mail->SMTPDebug    = 3;
+			$mail->SMTPSecure   = $_POST['smtp_secure'];
+			$mail->Port         = $_POST['smtp_port'];
+			$mail->Username     = $_POST['smtp_user'];
+			$mail->Password     = $_POST['smtp_pass'];
+			$mail->From         = $_POST['sender_from_mail'];
+			$mail->FromName     = $_POST['sender_author'];
+			$mail->AddReplyTo($_POST['sender_from_mail'], $_POST['sender_author']);//回复地址
+			$mail->AddAddress($_POST['sender_to_mail']);//收件人地址
+			$mail->Subject      = "phpmailer测试标题";
+			$mail->Body         = "<h1>phpmail演示</h1>这是php点点通（<font color=red>www.phpddt.com</font>）对phpmailer的测试内容";
+			$mail->AltBody      = "To view the message, please use an HTML compatible email viewer!"; //当邮件不支持html时备用显示，可以省略
+			$mail->WordWrap     = 250;
+			$mail->Send();
+		} catch (phpmailerException $e) {
+			echo "邮件发送失败：".$e->errorMessage();
 		}
 	}
 }
-
 $s = new send();
 $s->index();
